5fedcf895522df80289191826824c2ca34cec91d
Galuh Ramadhan <galuh.html@gmail.com>
2022-05-01 05:49:34 +0700

    responsive header

--- src/components/AccountMenu/MenuButton.js
+++ src/components/AccountMenu/MenuButton.js
@@ -1,4 +1,4 @@
-import { DotsVerticalIcon } from '@radix-ui/react-icons'
+import { DotsVerticalIcon } from '@radix-ui/react-icons';
 import CustomAvatar from "../CustomAvatar";
 
 const MenuButton = ({ onClick }) => (
@@ -7,7 +7,7 @@ const MenuButton = ({ onClick }) => (
             <div className="basis-1/4">
                 <CustomAvatar />
             </div>
-            <div className="basis-2/4 pl-2">
+            <div className="basis-2/4 pl-2 hidden sm:block">
                 <h5 className="mx-0 font-semibold text-sm whitespace-nowrap text-ellipsis overflow-hidden w-24">Enak Banget</h5>
                 <span className="text-xs subpixel-antialiased tracking-wide">superadmin</span>
             </div>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,19 @@
+import { HamburgerMenuIcon } from '@radix-ui/react-icons';
 import Logo from '../Logo';
 import AccountMenu from '../AccountMenu';
 
 export default () => {
     return (
-        <div className="shadow-lg fixed top-0 left-0 right-0 w-full bg-white py-1">
+        <div className="shadow-lg fixed top-0 left-0 right-0 w-full bg-white py-1 px-6">
             <div className="flex flex-row items-center">
-                <div className="basis-1/2"><Logo /></div>
+                <div className="basis-1/2 overflow-hidden">
+                    <div className="flex w-64 gap-2">
+                        <button type="button"><HamburgerMenuIcon /></button>
+                        <Logo />
+                    </div>
+                </div>
                 <div className="basis-1/2">
-                    <div className="float-right mr-10">
+                    <div className="float-right">
                         <AccountMenu />
                     </div>
                 </div>
