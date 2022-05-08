b0952bb3fd4938c5e580e733a4be037d151d4ae5
Galuh Ramadhan <galuh.html@gmail.com>
2022-05-08 08:16:58 +0700

    fix: css hover table row

--- src/components/Table/Body.js
+++ src/components/Table/Body.js
@@ -11,7 +11,7 @@ export default ({ columns, data, onRowClick }) => {
         <tbody className="divide-y divide-solid">
             {data && data.length === 0 && <EmptyRow {...{ columns }} />}
             {data && data.map((x, i) => (
-                <tr key={i} onClick={() => { onRowClick(x); }}>
+                <tr key={i} onClick={() => { onRowClick(x); }} className="hover:bg-slate-50 hover:cursor-pointer">
                     {columns.map(col => (<td className="py-2 px-1.5" key={`${i}${col.id}`}>{isHasProperty(col, "customComponent") ? col.customComponent(x[col.id], x) : x[col.id]}</td>))}
                 </tr>
             ))}
