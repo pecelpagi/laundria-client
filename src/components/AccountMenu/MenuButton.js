0f60673f87283cd16b117c07c011f32de8919e08
Galuh Ramadhan <galuh.html@gmail.com>
2022-05-27 18:29:08 +0700

    feat: skeleton header

--- src/components/AccountMenu/MenuButton.js
+++ src/components/AccountMenu/MenuButton.js
@@ -1,7 +1,22 @@
 import { useContext } from 'react';
+import ContentLoader from 'react-content-loader'
 import { DotsVerticalIcon, DotFilledIcon } from '@radix-ui/react-icons';
 import CustomAvatar from "../CustomAvatar";
 import { ComponentContext } from "../../mainlayout/Context";
+import React from 'react';
+
+const MyLoader = () => (
+    <ContentLoader
+        speed={2}
+        width={96}
+        height={38}
+        backgroundColor="#d9d9d9"
+        foregroundColor="#ededed"
+    >
+        <rect y="0" width="96" height="14" />
+        <rect y="23" width="50" height="12" />
+    </ContentLoader>
+)
 
 const MenuButton = ({ onClick }) => {
     const { loggedInProfile } = useContext(ComponentContext);
@@ -13,8 +28,15 @@ const MenuButton = ({ onClick }) => {
                     <CustomAvatar />
                 </div>
                 <div className="basis-2/4 pl-2 hidden sm:block">
-                    <h5 className="mx-0 font-semibold text-sm whitespace-nowrap text-ellipsis overflow-hidden w-24">{loggedInProfile ? loggedInProfile.fullname : ''}</h5>
-                    <span className="text-xs subpixel-antialiased tracking-wide flex items-center"><DotFilledIcon className="text-lime-700" height={18} width={18} />Online</span>
+                    {loggedInProfile && (
+                        <React.Fragment>
+                            <h5 className="mx-0 font-semibold text-sm whitespace-nowrap text-ellipsis overflow-hidden w-24">{loggedInProfile ? loggedInProfile.fullname : ''}</h5>
+                            <span className="text-xs subpixel-antialiased tracking-wide flex items-center"><DotFilledIcon className="text-lime-700" height={18} width={18} />Online</span>
+                        </React.Fragment>
+                    )}
+                    {!loggedInProfile && (
+                        <MyLoader />
+                    )}
                 </div>
                 <div className="basis-1/4">
                     <button type="button" id="dot-menu-button" className="pl-3 pr-0 py-2 sm:px-4" {...{ onClick }}><DotsVerticalIcon id="dot-menu-icon" /></button>
