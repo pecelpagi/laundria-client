37ad9e938d427145cb83cdc4d62e3ede32f1d802
Galuh Ramadhan <galuh.html@gmail.com>
2022-05-29 07:52:25 +0700

    feat: add spinner - identity

--- src/pages/Identity/index.js
+++ src/pages/Identity/index.js
@@ -5,11 +5,13 @@ import { ComponentContext } from "../../mainlayout/Context";
 import { catchError } from '../../utils';
 import { TOAST_TYPE } from '../../mainlayout/enums';
 import Form from "./Form";
+import Spinner from '../../components/Spinner';
 
 class Identity extends React.Component {
     state = {
         formData: null,
         readOnlyCompanyProfile: {},
+        isSaving: false,
     }
 
     static getDerivedStateFromProps(props, state) {
@@ -33,12 +35,16 @@ class Identity extends React.Component {
         ]);
     }
 
+    handleSetSaving = (isSaving) => { this.setState({ isSaving }); }
+
     doShowingNotification = (toastType, message) => {
         const { onShowNotification } = this.props;
         onShowNotification(toastType, message);
     }
 
     handleSaveData = async (data) => {
+        this.handleSetSaving(true);
+
         try {
             const { onFetchCompanyProfile } = this.props;
 
@@ -49,22 +55,27 @@ class Identity extends React.Component {
             this.doShowingNotification(TOAST_TYPE.SUCCESS, 'Data identitas aplikasi tersimpan');
         } catch (e) {
             this.doShowingNotification(TOAST_TYPE.ERROR, catchError(e));
+        } finally {
+            this.handleSetSaving(false);
         }
     }
 
     render() {
-        const { formData } = this.state;
+        const { formData, isSaving } = this.state;
 
         if (!formData) return null;
 
         return (
-            <div className="bg-white rounded divide-y divide-solid">
-                <div className="flex p-3 items-center">
-                    <div className="text-base font-semibold w-full">Identitas Aplikasi</div>
+            <div className="relative">
+                <div className="bg-white rounded divide-y divide-solid">
+                    <div className="flex p-3 items-center">
+                        <div className="text-base font-semibold w-full">Identitas Aplikasi</div>
+                    </div>
+                    <div className="p-3">
+                        <Form onSave={this.handleSaveData} {...{ formData }} />
+                    </div>
                 </div>
-                <div className="p-3">
-                    <Form onSave={this.handleSaveData} {...{ formData }} />
-                </div>
+                {isSaving && <Spinner />}
             </div>
         )
     }
